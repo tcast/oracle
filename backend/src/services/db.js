42cb12e319f0c4670b598348a68080b35d20ba1f
tcast <tony@qcfund.com>
2025-02-16 17:15:03 -0500

    working locally version

--- backend/src/services/db.js
+++ backend/src/services/db.js
@@ -1,21 +1,22 @@
 const { Pool } = require('pg');
-const dotenv = require('dotenv');
 
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const poolConfig = process.env.DATABASE_URL
-? {
-connectionString: process.env.DATABASE_URL,
-ssl: { rejectUnauthorized: false }
-}
-: {
-user: process.env.DB_USER,
-host: process.env.DB_HOST,
-database: process.env.DB_NAME,
-password: process.env.DB_PASSWORD,
-port: process.env.DB_PORT,
-ssl: false
-};
+  ? {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    }
+  : {
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
+      ssl: false
+    };
 
 const pool = new Pool(poolConfig);
 module.exports = pool;
